43cad7b21c6080c1d10a8c1d66a40865d7e1d630
Lincoln <lincolninsper@gmail.com>
2024-03-04 10:30:24 -0300

    puzzle 2 not yet finished :(

---
+++ EscapeRoom/Assets/XR_Socket_Tag_Interactor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSocketTagInteractor : XRSocketInteractor
+{
+    public string targetTag;
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
+}
