0f5b761fc97fcd61086ccacf5a5c2f6e3f769e03
Lincoln <lincolninsper@gmail.com>
2024-03-05 08:47:16 -0300

    lanterna na corrente se mexendo, ainda farei a dinamica de cair a lantern

--- EscapeRoom/Assets/Scripts/ActivateLaser.cs
+++ EscapeRoom/Assets/Scripts/ActivateLaser.cs
@@ -24,7 +24,7 @@ public class ActivateLaser : MonoBehaviour
         if (lineRenderer != null)
         {
             // Desativa o LineRenderer no início
-            lineRenderer.enabled = true;
+            lineRenderer.enabled = false;
         }
         else
         {
