fb5ac653b95a18093d494e32dd250d5741464e3b
LidiaDomingos <lidiaalves2002@outlook.com>
2024-03-05 15:48:12 -0300

    puzzle finished

--- EscapeRoom/Assets/RespawnGhost.cs
+++ EscapeRoom/Assets/RespawnGhost.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RespawnGhost : MonoBehaviour
 {
     public string targetTag;
     private Vector3 initialPosition;
+    public UnityEvent<GameObject> OnEnterEvent;
+    public GameObject ghost;
 
     private void Start()
     {
-        initialPosition = transform.position;
+        initialPosition = ghost.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == targetTag)
         {
-            transform.position = initialPosition;
+            OnEnterEvent.Invoke(other.gameObject);
+        }
+    }
+
+    public void Respawn()
+    {
+        if (initialPosition != null)
+        {
+            ghost.transform.position = initialPosition;
         }
     }
 }
